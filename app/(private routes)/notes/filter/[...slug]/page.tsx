4c402490194c95d50954e3c6947448f50da7152c
Veronika Kulynych <kulynychron@gmail.com>
2025-10-24 10:46:30 +0300

    fix params

--- app/(private routes)/notes/filter/[...slug]/page.tsx
+++ app/(private routes)/notes/filter/[...slug]/page.tsx
@@ -1,12 +1,71 @@
+// import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
+// import NotesClient from "./Notes.client";
+// import { fetchNotes } from "@/lib/api/clientApi";
+
+// import { Metadata } from "next";
+
+
+// export async function generateMetadata({ params }: Props): Promise<Metadata> {
+//   const tag = (await params).slug?.[0] || "All";
+//   const selectedTag = tag === "All" ? "All" : tag;
+
+//   return {
+//     title: `Notes tag: ${selectedTag} | NoteHub`,
+//     description: `Browse all notes tagged with "${selectedTag}" on NoteHub. Discover and manage notes related to this topic.`,
+//     openGraph: {
+//       title: `Notes tag: ${selectedTag} | NoteHub`,
+//       description: `View and explore notes marked with the tag "${selectedTag}". Find all your content related to this category in one place.`,
+//       url: `https://notehub.com/notes/filter/${selectedTag}`,
+//       siteName: "NoteHub",
+//       images: [
+//         {
+//           url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+//           width: 1200,
+//           height: 630,
+//           alt: `Notes tagged with ${selectedTag}`,
+//         },
+//       ],
+//       type: "website",
+//     },
+//   };
+// }
+
+// type Props = {
+//   params: Promise<{ slug?: string[] }>;
+// };
+
+// export default async function NotesPage({ params }: Props) {
+//   const { slug } = await params;
+//   const [tag] = slug || [];
+//   const page = 1;
+
+//   const queryClient = new QueryClient();
+
+//   await queryClient.prefetchQuery({
+//     queryKey: ["notes", { tag, page }],
+//     queryFn: () => fetchNotes({ tag, searchQuery: "", page }),
+//   });
+
+//   const dehydratedState = dehydrate(queryClient);
+
+//   return (
+//     <HydrationBoundary state={dehydratedState}>
+//       <NotesClient tag={tag}  />
+//     </HydrationBoundary>
+//   );
+// }
+
 import { QueryClient, dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import NotesClient from "./Notes.client";
 import { fetchNotes } from "@/lib/api/clientApi";
-
 import { Metadata } from "next";
 
+type Props = {
+  params: { slug?: string[] };
+};
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-  const tag = (await params).slug?.[0] || "All";
+  const tag = params.slug?.[0] || "All";
   const selectedTag = tag === "All" ? "All" : tag;
 
   return {
@@ -30,12 +89,8 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   };
 }
 
-type Props = {
-  params: Promise<{ slug?: string[] }>;
-};
-
 export default async function NotesPage({ params }: Props) {
-  const { slug } = await params;
+  const { slug } = params;
   const [tag] = slug || [];
   const page = 1;
 
@@ -50,7 +105,7 @@ export default async function NotesPage({ params }: Props) {
 
   return (
     <HydrationBoundary state={dehydratedState}>
-      <NotesClient tag={tag}  />
+      <NotesClient tag={tag} />
     </HydrationBoundary>
   );
 }
--- app/api/notes/[id]/route.ts
+++ app/api/notes/[id]/route.ts
@@ -1,3 +1,95 @@
+// import { NextResponse } from "next/server";
+// import { api } from "../../api";
+// import { cookies } from "next/headers";
+// import { logErrorResponse } from "../../_utils/utils";
+// import { isAxiosError } from "axios";
+
+// type Props = {
+//   params: Promise<{ id: string }>;
+// };
+
+// export async function GET(request: Request, { params }: Props) {
+//   try {
+//     const cookieStore = await cookies();
+//     const { id } = await params;
+//     const res = await api(`/notes/${id}`, {
+//       headers: {
+//         Cookie: cookieStore.toString(),
+//       },
+//     });
+//     return NextResponse.json(res.data, { status: res.status });
+//   } catch (error) {
+//     if (isAxiosError(error)) {
+//       logErrorResponse(error.response?.data);
+//       return NextResponse.json(
+//         { error: error.message, response: error.response?.data },
+//         { status: error.status }
+//       );
+//     }
+//     logErrorResponse({ message: (error as Error).message });
+//     return NextResponse.json(
+//       { error: "Internal Server Error" },
+//       { status: 500 }
+//     );
+//   }
+// }
+
+// export async function DELETE(request: Request, { params }: Props) {
+//   try {
+//     const cookieStore = await cookies();
+//     const { id } = await params;
+
+//     const res = await api.delete(`/notes/${id}`, {
+//       headers: {
+//         Cookie: cookieStore.toString(),
+//       },
+//     });
+//     return NextResponse.json(res.data, { status: res.status });
+//   } catch (error) {
+//     if (isAxiosError(error)) {
+//       logErrorResponse(error.response?.data);
+//       return NextResponse.json(
+//         { error: error.message, response: error.response?.data },
+//         { status: error.status }
+//       );
+//     }
+//     logErrorResponse({ message: (error as Error).message });
+//     return NextResponse.json(
+//       { error: "Internal Server Error" },
+//       { status: 500 }
+//     );
+//   }
+// }
+
+// export async function PATCH(request: Request, { params }: Props) {
+//   try {
+//     const cookieStore = await cookies();
+//     const { id } = await params;
+//     const body = await request.json();
+
+//     const res = await api.patch(`/notes/${id}`, body, {
+//       headers: {
+//         Cookie: cookieStore.toString(),
+//       },
+//     });
+//     return NextResponse.json(res.data, { status: res.status });
+//   } catch (error) {
+//     if (isAxiosError(error)) {
+//       logErrorResponse(error.response?.data);
+//       return NextResponse.json(
+//         { error: error.message, response: error.response?.data },
+//         { status: error.status }
+//       );
+//     }
+//     logErrorResponse({ message: (error as Error).message });
+//     return NextResponse.json(
+//       { error: "Internal Server Error" },
+//       { status: 500 }
+//     );
+//   }
+// }
+
+
 import { NextResponse } from "next/server";
 import { api } from "../../api";
 import { cookies } from "next/headers";
@@ -5,13 +97,13 @@ import { logErrorResponse } from "../../_utils/utils";
 import { isAxiosError } from "axios";
 
 type Props = {
-  params: Promise<{ id: string }>;
+  params: { id: string };
 };
 
 export async function GET(request: Request, { params }: Props) {
   try {
-    const cookieStore = await cookies();
-    const { id } = await params;
+    const cookieStore = cookies(); // не треба await
+    const { id } = params; // не треба await
     const res = await api(`/notes/${id}`, {
       headers: {
         Cookie: cookieStore.toString(),
@@ -36,8 +128,8 @@ export async function GET(request: Request, { params }: Props) {
 
 export async function DELETE(request: Request, { params }: Props) {
   try {
-    const cookieStore = await cookies();
-    const { id } = await params;
+    const cookieStore = cookies();
+    const { id } = params;
 
     const res = await api.delete(`/notes/${id}`, {
       headers: {
@@ -63,8 +155,8 @@ export async function DELETE(request: Request, { params }: Props) {
 
 export async function PATCH(request: Request, { params }: Props) {
   try {
-    const cookieStore = await cookies();
-    const { id } = await params;
+    const cookieStore = cookies();
+    const { id } = params;
     const body = await request.json();
 
     const res = await api.patch(`/notes/${id}`, body, {
