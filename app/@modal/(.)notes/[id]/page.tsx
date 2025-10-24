e390afd655a5d98ffd30f59f00a7b31b24970d60
Veronika Kulynych <kulynychron@gmail.com>
2025-10-24 10:32:55 +0300

    fix

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,18 +1,42 @@
-import { fetchNoteById } from "@/lib/api/clientApi";
-import NotePreviewModal from "./NotePreview.client";
-import { QueryClient, dehydrate } from "@tanstack/react-query";
+// import { fetchNoteById } from "@/lib/api/clientApi";
+// import NotePreviewModal from "./NotePreview.client";
+// import { QueryClient, dehydrate } from "@tanstack/react-query";
 
-type Props = {
-  params: { id: string };
-};
+// type Props = {
+//   params: { id: string };
+// };
 
-// const NotePreview = async ({ params }: Props) => {
+// // const NotePreview = async ({ params }: Props) => {
+// //   const queryClient = new QueryClient();
+// //   const { id } =  params;
+// //   await queryClient.prefetchQuery({
+// //     queryKey: ["note", id],
+// //     queryFn: () => fetchNoteById(id),
+// //   });
+// export default async function NotePreview({
+//   params,
+// }: {
+//   params: { id: string };
+// }) {
 //   const queryClient = new QueryClient();
-//   const { id } =  params;
+//   const { id } = params;
+
 //   await queryClient.prefetchQuery({
 //     queryKey: ["note", id],
 //     queryFn: () => fetchNoteById(id),
 //   });
+
+//   const dehydratedState = dehydrate(queryClient);
+
+//   return <NotePreviewModal dehydratedState={dehydratedState} />;
+// };
+
+// export default NotePreview;
+
+import { fetchNoteById } from "@/lib/api/clientApi";
+import NotePreviewModal from "./NotePreview.client";
+import { QueryClient, dehydrate } from "@tanstack/react-query";
+
 export default async function NotePreview({
   params,
 }: {
@@ -29,6 +53,4 @@ export default async function NotePreview({
   const dehydratedState = dehydrate(queryClient);
 
   return <NotePreviewModal dehydratedState={dehydratedState} />;
-};
-
-// export default NotePreview;
+}
